78b9e992352462416f6413ef50f1ef2ee8f60277
deaconbourn <deaconbourn@gmail.com>
2020-01-23 09:12:55 +0800

    update for turtling

---
+++ turtle demo 2.py
@@ -0,0 +1,21 @@
+import time
+import turtle
+import random
+
+turtle = turtle.Pen()
+
+for i in range(20):
+	turtle.right(45)
+	turtle.forward(100)
+	time.sleep(2)
+	turtle.speed(0)
+turtle.reset()
+a =  random.randrange(20)
+while True:
+	for randomization in a:
+		turtle.circle(20)
+		turtle.forward(50)
+		turtle.right(15)
+	
+
+
